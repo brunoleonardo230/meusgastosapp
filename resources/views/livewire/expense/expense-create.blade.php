84aaf9037f974cdd58aacf8b46bbe59f06a24ce5
Bruno Leonardo <bruno.leonardo23@hotmail.com>
2021-08-20 22:51:01 -0300

    preview fotos

--- resources/views/livewire/expense/expense-create.blade.php
+++ resources/views/livewire/expense/expense-create.blade.php
@@ -49,6 +49,9 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Foto Comprovante</label>
                 <input type="file" name="amount" wire:model="photo"
                        class="block appearance-none w-full bg-gray-200 border @error('photo') border-red-500 @else border-gray-200 @enderror  text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                @if($photo)
+                    <img src="{{$photo->temporaryUrl()}}" alt="" width="150px" class="my-3">
+                @endif
 
             @error('photo')
             <h5 class="text-red-500 text-xs italic">{{$message}}</h5>
