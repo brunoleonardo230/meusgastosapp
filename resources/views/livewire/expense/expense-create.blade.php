29824c95826611ee944be97926f964b839244702
Bruno Leonardo <bruno.leonardo23@hotmail.com>
2021-08-20 23:12:14 -0300

    data comprovante

--- resources/views/livewire/expense/expense-create.blade.php
+++ resources/views/livewire/expense/expense-create.blade.php
@@ -47,7 +47,7 @@
 
             <p class="w-full px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Foto Comprovante</label>
-                <input type="file" name="amount" wire:model="photo"
+                <input type="file" name="photo" wire:model="photo"
                        class="block appearance-none w-full bg-gray-200 border @error('photo') border-red-500 @else border-gray-200 @enderror  text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                 @if($photo)
                     <img src="{{$photo->temporaryUrl()}}" alt="" width="150px" class="my-3">
@@ -56,6 +56,13 @@
             @error('photo')
             <h5 class="text-red-500 text-xs italic">{{$message}}</h5>
             @enderror
+            </p>
+
+            <p class="w-full px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Data do comprovante, pode deixar em branco se o dia for hoje.</label>
+                <input type="text" name="expense_date" wire:model="expenseDate"
+                       class="block appearance-none w-full bg-gray-200 border text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+
             </p>
 
         </div>
